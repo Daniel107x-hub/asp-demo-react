9b7cd7da5f8633045e9e6c633f19910f249d8dc7
daniel107x-hub <dmejiav310796@outlook.com>
2024-06-12 10:51:35 -0600

    Enabled HTTPS communication

--- client/src/services/Login/LoginService.tsx
+++ client/src/services/Login/LoginService.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const login = (email: String, password: String) => {
-    return axios.post('http://localhost:5104/login', { email, password });
+    return axios.post('https://localhost:5105/login', { email, password });
     // return new Promise((resolve, reject) => {
     //     setTimeout(() => {
     //     if (email === 'admin' && password === 'admin') {
