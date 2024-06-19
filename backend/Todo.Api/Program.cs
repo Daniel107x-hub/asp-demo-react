8cfcd5d73334930086c464bfeb9d9eb6a33e462f
daniel107x-hub <dmejiav310796@outlook.com>
2024-06-19 10:49:58 -0600

    Added swagger authorization

--- backend/Todo.Api/Program.cs
+++ backend/Todo.Api/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Todo.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +13,32 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option => {
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 builder.Services.AddRouting(option => option.LowercaseUrls = true);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>();
@@ -26,7 +52,7 @@ if(builder.Environment.IsDevelopment())
     builder.Services.AddCors(options => {
         options.AddPolicy(name: specificOrigins,
             policy => {
-                policy.WithOrigins("http://localhost:3000", "https://localhost:3000")
+                policy.WithOrigins("http://localhost:3000", "https://localhost:3000", "http://localhost:5104")
                     .AllowAnyHeader()
                     .AllowAnyMethod()
                     .AllowCredentials();
